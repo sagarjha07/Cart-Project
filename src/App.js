f05726e97374cc27edf143ce0db1318bcc4bac3e
Sagar kumar jha <jha8768@gmail.com>
2021-01-20 06:02:35 +0530

    styles added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import CartItem from "./cartItem";
+
+function App() {
+    return (
+        <div className="App">
+            <h1>CART</h1>
+            <CartItem />
+        </div>
+    );
+}
+
+export default App;
